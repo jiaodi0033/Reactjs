5b7e24190f5293165974d925df688128eb6c7067
23220 <2322009884@qq.com>
2020-11-09 21:56:09 +0800

    feat:轮播图实现

--- Carousel/src/csrousel.js
+++ Carousel/src/csrousel.js
@@ -9,10 +9,10 @@ pre.onclick=function (){
 }
 function next_pic(){
     let newLeft;
-    if(wrap.style.left==='-3600px'){
-        newLeft=-1200;
+    if(wrap.style.left==='-800px'){
+        newLeft=-200;
     }else {
-        newLeft=parseInt(wrap.style.left)-600;
+        newLeft=parseInt(wrap.style.left)-200;
     }
     wrap.style.left=newLeft+'px';
     index++;
@@ -23,10 +23,10 @@ function next_pic(){
 }
 function pre_pic(){
     let newLeft;
-    if(wrap.style.left==='-600px'){
-        newLeft=-2400;
+    if(wrap.style.left==='-200px'){
+        newLeft=-800;
     }else {
-        newLeft=parseInt(wrap.style.left)+600;
+        newLeft=parseInt(wrap.style.left)+200;
     }
     wrap.style.left=newLeft+'px';
     index--;
@@ -39,6 +39,7 @@ let timer=null;
 var index=0;
 let carousel=document.querySelector('.header');
 var dots=document.getElementsByTagName('span');
+var curPic=document.getElementsByTagName('img');
 carousel.onmouseenter=function (){
     clearInterval(timer);
 }
@@ -57,20 +58,24 @@ function showCurrentDot(){
     for (var i=0;i<dots.length;i++){
         dots[i].className='';
     }
+    for (var i=0;i<curPic.length;i++){
+        curPic[i].style.height='400px';
+    }
     dots[index].className='on';
+    curPic[index].style.height='300px';
 }
 for (var i = 0, len = dots.length; i < len; i++){
     (function(i){
         dots[i].onclick = function () {
             var dis = index - i;
-            if(index == 4 && parseInt(wrap.style.left)!==-3000){
+            if(index == 4 && parseInt(wrap.style.left)!==-1000){
                 dis = dis - 5;
             }
             //和使用prev和next相同，在最开始的照片5和最终的照片1在使用时会出现问题，导致符号和位数的出错，做相应地处理即可
-            if(index == 0 && parseInt(wrap.style.left)!== -600){
+            if(index == 0 && parseInt(wrap.style.left)!== -200){
                 dis = 5 + dis;
             }
-            wrap.style.left = (parseInt(wrap.style.left) +  dis * 600)+"px";
+            wrap.style.left = (parseInt(wrap.style.left) +  dis * 200)+"px";
             index = i;
             showCurrentDot();
         }
